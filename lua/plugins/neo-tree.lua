7c1095ddd4a526c106f622b0b0781ff936841167
PedroPianna <98548259+PedroPianna@users.noreply.github.com>
2025-09-25 13:42:50 +0200

    toggle hidden files neo tree using H

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,5 +9,20 @@ return {
 	config = function()
 		vim.keymap.set("n", "<C-n>", ":Neotree toggle right<CR>", {})
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+        require("neo-tree").setup({
+            filesystem = {
+                filtered_items = {
+                    visible = false, -- false = hide hidden files by default
+                    show_hidden_count = true,
+                    hide_dotfiles = true,
+                    hide_gitignored = true,
+                },
+                window = {
+                    mappings = {
+                        ["H"] = "toggle_hidden", -- press H in the Neo-tree window to toggle hidden files
+                    },
+                },
+            },
+        })
 	end,
 }
